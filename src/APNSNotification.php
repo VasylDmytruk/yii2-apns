19803ec8d162edabae29f133a5326211700378f5
Vasyl Dmytruk <vasyl.dmytruk@xloo.com>
2018-09-12 17:03:05 +0300

    First commit.

---
+++ src/APNSNotification.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace autoxloo\yii2\apns;
+
+use yii\base\Component;
+
+/**
+ * Class APNSNotification
+ */
+class APNSNotification extends Component
+{
+    public static function test()
+    {
+        echo __METHOD__ . PHP_EOL;
+    }
+}
